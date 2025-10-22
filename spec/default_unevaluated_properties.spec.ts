25bb1f7149226ed4cfbbb5245b531cc1d635ed11
Vadym Vasylyshyn <51933329+vadyvas@users.noreply.github.com>
2025-10-22 13:50:01 +0300

    fix: enhance unevaluatedProperties handling for composite rules

--- spec/default_unevaluated_properties.spec.ts
+++ spec/default_unevaluated_properties.spec.ts
@@ -191,6 +191,75 @@ describe("defaultUnevaluatedProperties=false option", function () {
       assertInvalid(schemas, {foo: {baz: 1}})
       assertInvalid(schemas, {foo: {bar: "string", baz: 1}})
     })
+
+    it("should work correctly with anyOf", () => {
+      const schema1 = {
+        type: "object",
+        properties: {
+          address: {type: "string"},
+        },
+        anyOf: [
+          {
+            type: "object",
+            required: ["firstName", "lastName"],
+            properties: {
+              firstName: {type: "string"},
+              lastName: {type: "string"},
+            },
+          },
+          {
+            type: "object",
+            required: ["name"],
+            properties: {
+              name: {type: "string"},
+            },
+          },
+        ],
+      }
+
+      const schemas = [schema1]
+      assertInvalid(schemas, {})
+      assertValid(schemas, {address: "someWhere", firstName: "John", lastName: "Doe"})
+      assertValid(schemas, {address: "someWhere", name: "John Doe"})
+      assertInvalid(schemas, {address: "someWhere", firstName: "John"})
+      assertInvalid(schemas, {address: "someWhere", foo: 1})
+      assertValid(schemas, {firstName: "John", lastName: "Doe"})
+    })
+
+    it("should work correctly with oneOf", () => {
+      const schema1 = {
+        type: "object",
+        properties: {
+          address: {type: "string"},
+        },
+        oneOf: [
+          {
+            type: "object",
+            required: ["firstName", "lastName"],
+            properties: {
+              firstName: {type: "string"},
+              lastName: {type: "string"},
+            },
+          },
+          {
+            type: "object",
+            required: ["name"],
+            properties: {
+              name: {type: "string"},
+            },
+          },
+        ],
+      }
+
+      const schemas = [schema1]
+
+      assertInvalid(schemas, {})
+      assertValid(schemas, {address: "someWhere", firstName: "John", lastName: "Doe"})
+      assertValid(schemas, {address: "someWhere", name: "John Doe"})
+      assertInvalid(schemas, {firstName: "John", lastName: "Doe", name: "JD"})
+      assertInvalid(schemas, {address: "someWhere", foo: 1})
+      assertValid(schemas, {firstName: "John", lastName: "Doe"})
+    })
   })
 
   describe("validation with $refs", () => {
@@ -296,8 +365,16 @@ describe("defaultUnevaluatedProperties=false option", function () {
   })
 
   function assertValid(schemas: SchemaObject[], data: unknown): void {
-    schemas.forEach((schema) =>
-      ajvs.forEach((ajv) => assert.strictEqual(ajv.validate(schema, data), true))
+    schemas.forEach(
+      (schema) => ajvs.forEach((ajv) => assert.strictEqual(ajv.validate(schema, data), true))
+      // ajvs.forEach((ajv) => {
+      //   console.log(JSON.stringify(schema, null, 2));
+      //   console.log(data);
+      //   const a = ajv.validate(schema, data);
+      //   console.log(a);
+      //   console.log(ajv.errors)
+      //   assert.strictEqual(a, true)
+      // })
     )
   }
 
